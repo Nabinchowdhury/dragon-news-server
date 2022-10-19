df819431a12df3a6c4d3883c02af067e2e6c7983
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-20 01:21:35 +0600

    news/:id created

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categoreies = require("./data/categories.json")
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
     res.send("News API running")
@@ -13,6 +14,23 @@ app.get('/', (req, res) => {
 
 app.get('/news-category', (req, res) => {
     res.send(categoreies)
+})
+
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/category/:id', (req, res) => {
+    const categoryId = req.params.id
+    if (categoryId === "08") {
+        res.send(news)
+    }
+    else {
+        const selectedCategory = news.filter(n => n.category_id === categoryId)
+        res.send(selectedCategory)
+    }
 })
 
 app.listen(port, () => {
